513c3cb89ffbf3fa9574a9bf880c2f11a94abd9d
Heloisa Rangel <heloisa.guimares@aluno.senai.br>
2024-05-14 16:11:22 -0300

    Trabalhando com concessionaria

--- 05_08/js/Concessionaria.js
+++ 05_08/js/Concessionaria.js
@@ -1,16 +1,27 @@
-import {Carro} from './Carro'
+import {Carro} from './Carro.js'
 
- export class Concessionaria{
+ export default class Concessionaria{
  constructor(){
+    // ao usar o this nos criamos ou manipulamos arrays e/ou variaveis globais
     this.carros = []
- }
+    let uno = new Carro("fiat","xxy-4502", "vermelho", 2012)
+    this.carros.push(uno)
+
+    this.carros.push(new Carro("Volkswagen", "uuu-8978", "branco", 1980))
+    this.carros.push(new Carro("fiat", "xxx-2020", "branco", 2014))
+    this.carros.push(new Carro("chevrolet", "xxx-2020", "branco", 2023))
+}
+
  removerCarro(index){
+    alert(this.carros[index]+"Deletado")
     // o remoto splice altera o conteudo de uma lista podendo adicionar enquanto remove elemento
     // abaixo vamos remover um elemento especifico
     
      this.carros.splice(index,1)
     //index preenta a posicao do array e o numero a quantidade de itens 
-    carregarCarros()
+    this.carregarCarros()
+     
+
     }
 
     carregarCarros() {
@@ -22,14 +33,14 @@ import {Carro} from './Carro'
             html+="<th>Deletar</th></tr>"
     
     
-        for (let i = 0; i < carros.length; i++) {
+        for (let i = 0; i < this.carros.length; i++) {
             html += "<tr>"
             html += "<td>"+ i + "</td>"
-            html += "<td>" + carros[i].marca + "</td>"
-            html += "<td>" + carros[i].placa + "</td>"
-            html += "<td>" + carros[i].cor + "</td>"
-            html += "<td>" + carros[i].ano + "</td>"
-            html += '<td><button onclick = "removerCarro('+i+')">Deletar</button></td>'
+            html += "<td>" + this.carros[i].marca + "</td>"
+            html += "<td>" + this.carros[i].placa + "</td>"
+            html += "<td>" + this.carros[i].cor + "</td>"
+            html += "<td>" + this.carros[i].ano + "</td>"
+            html += '<td><button onclick = "window.novaConcessionaria.removerCarro('+i+')">Deletar</button></td>'
             html += "</tr>"
         }
         document.getElementById("listaCarros").innerHTML = html
@@ -43,8 +54,15 @@ import {Carro} from './Carro'
     
         let carro = new Carro(marca, placa, cor, ano)
     
-        carros.push(carro)
-        carregarCarros()
+        this.carros.push(carro)
+        alert(this.carro[this.carros.length-1].marca + "Cadastrado com sucesso")
+        this.carregarCarros()
+        this.limparCampos()
     }
-
+     limparCampos(){
+        let marca = document.getElementById("marca").value =""
+        let placa = document.getElementById("placa").value= ""
+        let cor = document.getElementById("cor").value = "" 
+        let ano = document.getElementById("ano").value= ""
+     }
 }
